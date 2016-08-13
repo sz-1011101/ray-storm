29f638f2239e83eab7a54e2729cfabc52a820328
sebastian <Sebastian Z>
2016-08-13 17:42:05 +0200

    added Plane primitve

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "scene/Scene.h"
 #include "geometry/Sphere.hpp"
+#include "geometry/Plane.hpp"
 #include "camera/PinholeCamera.h"
 #include "materials/Lambertian.hpp"
 #include "renderer/PathTracer.h"
@@ -13,14 +14,22 @@ int main(int argc, char* argv[])
 
   scene::ScenePtr scene(new scene::Scene());
 
+  // camera
   camera::AbstractCameraPtr camera(new camera::PinholeCamera(glm::vec3(-7,5,5), glm::vec3(0,0,0), glm::vec3(0, 1, 0), 1.0f, 75.0f));
-  materials::AbstractMaterialPtr mat1(new materials::Lambertian(glm::vec3(1.0), glm::vec3(0.0f)));
-  materials::AbstractMaterialPtr mat2(new materials::Lambertian(glm::vec3(0.4), glm::vec3(10.0f)));
+  
+  // materials
+  materials::AbstractMaterialPtr mat1(new materials::Lambertian(glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f)));
+  materials::AbstractMaterialPtr mat2(new materials::Lambertian(glm::vec3(0.4f), glm::vec3(10.0f)));
+  materials::AbstractMaterialPtr mat3(new materials::Lambertian(glm::vec3(1.0f), glm::vec3(0.0f)));
 
+  // scene objects
   geometry::ObjectPtr sphere1 = geometry::ObjectPtr(new geometry::Sphere(glm::vec3(1.0f, 0.0, 0.0f), 1.0f, mat1));
   geometry::ObjectPtr sphere2 = geometry::ObjectPtr(new geometry::Sphere(glm::vec3(-0.5f, 0.0f, 0.0f), 0.25f, mat2));
+  geometry::ObjectPtr plane = geometry::ObjectPtr(new geometry::Plane(glm::vec3(0.0f, -1.0f, 0.0f), glm::vec3(0, 1, 0), mat3));
+
   scene->add(sphere1);
   scene->add(sphere2);
+  scene->add(plane);
   scene->finalize();
 
   utility::RenderedDataPtr rd(new utility::RenderedData());
