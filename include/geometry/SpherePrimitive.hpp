bc9f2a7280c82254b2ff046c6d87851898495f81
Sebastian Zahn <zahn.sebastian@googlemail.com>
2016-11-05 20:57:09 +0100

    SpherePrimitive added for simple sphere operations

---
+++ include/geometry/SpherePrimitive.hpp
@@ -0,0 +1,74 @@
+#ifndef SPHERE_PRIMITIVE_H_
+#define SPHERE_PRIMITIVE_H_
+
+#include "geometry/Intersectable.h"
+
+namespace ray_storm
+{
+  namespace geometry
+  {
+    class SpherePrimitive : public Intersectable<SpherePrimitive>
+    {
+    public:
+
+      SpherePrimitive(const glm::vec3 &position, float radius) : position(position), radius(radius)
+      {
+
+      }
+
+      inline bool intersect(const Ray &ray, Intersection<SpherePrimitive> &intersection)
+      {
+        const float a = glm::dot(ray.direction, ray.direction);
+        const glm::vec3 orgMinusCenter = ray.origin - this->position;
+        const float b = 2.0f*glm::dot(ray.direction, orgMinusCenter);
+        const float c = glm::dot(orgMinusCenter, orgMinusCenter) - this->radius*this->radius;
+
+        const float discriminant = b*b - 4*a*c;
+
+        // ray does not hit the sphere
+        if (discriminant < 0.0f) {
+          return false;
+        }
+
+        // ray hits...
+        const float sqrtDiscriminant = std::sqrt(discriminant);
+        const float t1 = (-b + sqrtDiscriminant)/(2.0f*a);
+        const float t2 = (-b - sqrtDiscriminant)/(2.0f*a);
+
+        // sphere is intersected but behind us
+        if (t1 < 0.0f && t2 < 0.0f) {
+          return false;
+        }
+
+        // sphere is hit
+        intersection.t = t1 > 0.0f && t2 > 0.0f ? std::min(t1, t2) : std::max(t1, t2);
+        intersection.intersected = this;
+        const glm::vec3 iPoint = ray.origin + intersection.t*ray.direction;
+        
+        const glm::vec3 spherical = utility::Math::cartesianToSpherical(iPoint - position);
+        intersection.intersection = SimpleIntersection (iPoint, glm::normalize(iPoint - this->position),
+          glm::vec2(spherical.y/(2.0f*M_PI), spherical.z/M_PI));
+        return true;
+      }
+
+      const glm::vec3 &getPosition() const
+      {
+        return this->position;
+      }
+
+      float getRadius() const
+      {
+        return this->radius;
+      }
+
+    protected:
+
+      glm::vec3 position;
+
+      float radius;
+      
+    };
+  }
+}
+
+#endif
--- include/objects/Sphere.hpp
+++ include/objects/Sphere.hpp
@@ -3,6 +3,7 @@
 
 #include "objects/Emitter.h"
 #include "utility/common.hpp"
+#include "geometry/SpherePrimitive.hpp"
 
 namespace ray_storm
 {
@@ -18,61 +19,38 @@ namespace ray_storm
         const glm::vec3 &position, 
         float radius, const materials::MaterialPtr &material,
         const glm::vec3 &emittance = glm::vec3(0.0f)
-      ) : Emitter(material, emittance)
+      ) : Emitter(material, emittance), sphere(position, radius)
       {
-        this->position = position;
-        this->radius = radius;
         this->bbox = geometry::AxisAlignedBox(position - glm::vec3(radius), glm::vec3(2.0f*radius));
       }
 
-      inline bool intersect(const geometry::Ray &ray, geometry::Intersection<Object> &intersection) {
-        const float a = glm::dot(ray.direction, ray.direction);
-        const glm::vec3 orgMinusCenter = ray.origin - this->position;
-        const float b = 2.0f*glm::dot(ray.direction, orgMinusCenter);
-        const float c = glm::dot(orgMinusCenter, orgMinusCenter) - this->radius*this->radius;
-
-        const float discriminant = b*b - 4*a*c;
-
-        // ray does not hit the sphere
-        if (discriminant < 0.0f) {
-          return false;
+      bool intersect(const geometry::Ray &ray, geometry::Intersection<Object> &intersection) {
+        geometry::Intersection<geometry::SpherePrimitive> _intersection;
+        if (this->sphere.intersect(ray, _intersection))
+        {
+          intersection.intersection = _intersection.intersection;
+          intersection.intersected = this;
+          intersection.t = _intersection.t;
+          return true;
         }
-
-        // ray hits...
-        const float sqrtDiscriminant = std::sqrt(discriminant);
-        const float t1 = (-b + sqrtDiscriminant)/(2.0f*a);
-        const float t2 = (-b - sqrtDiscriminant)/(2.0f*a);
-
-        // sphere is intersected but behind us
-        if (t1 < 0.0f && t2 < 0.0f) {
-          return false;
-        }
-
-        // sphere is hit
-        intersection.t = t1 > 0.0f && t2 > 0.0f ? std::min(t1, t2) : std::max(t1, t2);
-        intersection.intersected = this;
-        const glm::vec3 iPoint = ray.origin + intersection.t*ray.direction;
-        
-        const glm::vec3 spherical = utility::Math::cartesianToSpherical(iPoint - position);
-        intersection.intersection = geometry::SimpleIntersection (iPoint, glm::normalize(iPoint - this->position),
-          glm::vec2(spherical.y/(2.0f*M_PI), spherical.z/M_PI));
-        return true;
+        return false;
       }
 
       float getSurfaceArea()
       {
-        return 4.0f*static_cast<float>(M_PI)*this->radius*this->radius;
+        float rad = this->sphere.getRadius();
+        return 4.0f*static_cast<float>(M_PI)*rad*rad;
       }
 
       glm::vec3 drawRandomSurfacePoint(random::RandomizationHelper &randHelper)
       {
-        return this->position + randHelper.drawUniformRandomSphereDirection()*(this->radius + Emitter::SURFACE_POINT_OFFSET);
+        return this->sphere.getPosition() + randHelper.drawUniformRandomSphereDirection()*(this->sphere.getRadius() + Emitter::SURFACE_POINT_OFFSET);
       }
 
       void drawRandomRay(random::RandomizationHelper &randHelper, random::RandomRay &randRay)
       {
         randRay.ray.origin = this->drawRandomSurfacePoint(randHelper);
-        randRay.ray.direction = randHelper.drawUniformRandomHemisphereDirection(glm::normalize(randRay.ray.origin - this->position));
+        randRay.ray.direction = randHelper.drawUniformRandomHemisphereDirection(glm::normalize(randRay.ray.origin - this->sphere.getPosition()));
         randRay.PDF = this->getPDF()*random::RandomizationHelper::uniformRandomHemispherePDF();
       }
 
@@ -83,9 +61,7 @@ namespace ray_storm
 
     protected:
 
-      glm::vec3 position;
-
-      float radius;
+      geometry::SpherePrimitive sphere;
 
     };
   }
