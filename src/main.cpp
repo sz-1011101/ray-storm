a2d3aa0ac266fdbeca79c7f0e84bb7c760bb46fc
Sebastian Zahn <zahn.sebastian@googlemail.com>
2016-12-21 18:59:58 +0100

    method parameter added

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <sstream>
 
 #include "boost/program_options.hpp" 
 
@@ -10,22 +11,50 @@
 #include "camera/LensFactory.h"
 
 #include "scene/TestSceneFactory.h"
-#include "integrators/PathTracer.h"
+#include "integrators/IntegratorFactory.h"
 #include "renderer/DefaultRenderer.h"
 #include "utility/Window.h"
 #include "utility/RenderedData.h"
 
+using namespace ray_storm;
+
 const std::string SPP_ARG = "spp";
+const std::string METHOD_ARG = "method";
+const std::string METHOD_PT = "PT";
+const std::string METHOD_DPT = "DPT";
+const std::string METHOD_D_MIS_PT = "D_MIS_PT";
+const std::string METHOD_BIDIR_PT = "BIDIR_PT";
+
+integrators::AbstractIntegratorPtr parseMethod(const std::string &name)
+{
+  if (name == METHOD_PT)
+  {
+    return integrators::IntegratorFactory::createPathTracer();
+  }
+  else if (name == METHOD_DPT)
+  {
+    return integrators::IntegratorFactory::createDirectLightingPathTracer(false);
+  }
+  else if (name == METHOD_D_MIS_PT)
+  {
+    return integrators::IntegratorFactory::createDirectLightingPathTracer(true);
+  }
+  else if (name == METHOD_BIDIR_PT)
+  {
+    return integrators::IntegratorFactory::createBidirectionalPathTracer();
+  }
+
+  return integrators::AbstractIntegratorPtr();
+}
 
 int main(int argc, char* argv[])
 {
 
-  using namespace ray_storm;
-
   boost::program_options::options_description desc("Options");
   desc.add_options()
   ("help", "show help message")
-  (SPP_ARG.c_str(), boost::program_options::value<int>(), "samples per pixel");
+  (SPP_ARG.c_str(), boost::program_options::value<int>(), "samples per pixel")
+  (METHOD_ARG.c_str(), boost::program_options::value<std::string>(), "integration method."); // TODO show methods
 
   boost::program_options::variables_map vm;
   boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
@@ -38,10 +67,28 @@ int main(int argc, char* argv[])
   }
 
   int spp = 10;
+  integrators::AbstractIntegratorPtr pts;
+
   if (vm.count(SPP_ARG.c_str()))
   {
     spp = vm[SPP_ARG.c_str()].as<int>();
     std::cout << "samples per pixel set to " << spp << '\n';
+  }
+
+  if (vm.count(METHOD_ARG.c_str()))
+  {
+    const std::string method = vm[METHOD_ARG.c_str()].as<std::string>();
+    std::cout << "integration method set to " << method << '\n';
+    pts = parseMethod(method);
+    if (pts == nullptr)
+    {
+      std::cout << "method not recognized\n";
+      return -1;
+    }
+  }
+  else
+  {
+    pts = integrators::IntegratorFactory::createPathTracer();
   }
 
   utility::RenderedDataPtr rd(new utility::RenderedData());
@@ -65,7 +112,7 @@ int main(int argc, char* argv[])
     )
   );
 
-  integrators::AbstractIntegratorPtr pts(new integrators::PathTracer());
+  
 
   scene::ScenePtr scene = scene::TestSceneFactory::createCornellBox(false, true);
   
