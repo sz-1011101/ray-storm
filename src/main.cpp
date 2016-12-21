49cce3fec3ead2fa030f5eacb19e8cd0ee8ff33f
Sebastian Zahn <zahn.sebastian@googlemail.com>
2016-12-21 17:08:51 +0100

    added simple spp cmd line option via boost::program_options

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#include <string>
+
+#include "boost/program_options.hpp" 
+
 #include "utility/common.hpp"
 #include "scene/Scene.h"
 
@@ -11,10 +15,34 @@
 #include "utility/Window.h"
 #include "utility/RenderedData.h"
 
+const std::string SPP_ARG = "spp";
+
 int main(int argc, char* argv[])
 {
 
   using namespace ray_storm;
+
+  boost::program_options::options_description desc("Options");
+  desc.add_options()
+  ("help", "show help message")
+  (SPP_ARG.c_str(), boost::program_options::value<int>(), "samples per pixel");
+
+  boost::program_options::variables_map vm;
+  boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+  boost::program_options::notify(vm);
+
+  if (vm.count("help"))
+  {
+    std::cout << "ray-storm - Yet Another CPU Path Tracer\n"<< desc << '\n';
+    return 0;
+  }
+
+  int spp = 10;
+  if (vm.count(SPP_ARG.c_str()))
+  {
+    spp = vm[SPP_ARG.c_str()].as<int>();
+    std::cout << "samples per pixel set to " << spp << '\n';
+  }
 
   utility::RenderedDataPtr rd(new utility::RenderedData());
   utility::Window window;
@@ -43,7 +71,7 @@ int main(int argc, char* argv[])
 
   scene::ScenePtr scene = scene::TestSceneFactory::createCornellBox(false, true);
   
-  renderer::DefaultRenderer dr(scene, camera, pts, 10);
+  renderer::DefaultRenderer dr(scene, camera, pts, spp);
 
   dr.render();
 
