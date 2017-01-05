e78af5d464efe210fce65edf7e7df9fa8f61692c
Sebastian Zahn <zahn.sebastian@googlemail.com>
2017-01-05 21:44:42 +0100

    comment for the normal modification added

--- src/geometry/ModifiableSurface.cpp
+++ src/geometry/ModifiableSurface.cpp
@@ -18,6 +18,8 @@ void ModifiableSurface::modifyNormal(
     return;
   }
   const glm::vec3 modiN = this->map->modifyNormal(intersection.normal, intersection.texCoords);
+  // it could happen that the normal is changed below the viewing direction's plane, which will produce ugly artifacts,
+  // in this case, we just use the unmodified normal 
   if (std::signbit(glm::dot(direction, intersection.normal)) == std::signbit(glm::dot(direction, modiN)))
   {
     intersection.normal = modiN;
