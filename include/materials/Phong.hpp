4f3d21dfe486cb0ded1bba98e1359d70a579cf59
Sebastian Zahn <zahn.sebastian@googlemail.com>
2017-01-05 12:22:33 +0100

    reenabled adaptive pdf for Phong brdf

--- include/materials/Phong.hpp
+++ include/materials/Phong.hpp
@@ -52,11 +52,18 @@ namespace ray_storm
         random::RandomizationHelper &randHelper, 
         random::RandomDirection &randDir)
       {
-
-        glm::vec3 r = glm::normalize(glm::reflect(in, n));
-        const float e_uv = this->e->sample(uv);
-        randDir.direction = randHelper.drawCosineWeightedRandomHemisphereDirection(r, e_uv);
-        randDir.PDF = this->getPDF(in, n, uv, randDir.direction);
+        if (randHelper.drawUniformRandom() < this->computeCosineSamplingProbability(uv))
+        {
+          glm::vec3 r = glm::normalize(glm::reflect(in, n));
+          const float e_uv = this->e->sample(uv);
+          randDir.direction = randHelper.drawCosineWeightedRandomHemisphereDirection(r, e_uv);
+          randDir.PDF = this->getPDF(in, n, uv, randDir.direction);
+        }
+        else
+        {
+          randDir.direction = randHelper.drawCosineWeightedRandomHemisphereDirection(n, 1.0f);
+          randDir.PDF = this->getPDF(in, n, uv, randDir.direction);
+        }
       }
 
       float getPDF(
@@ -68,7 +75,9 @@ namespace ray_storm
       {
         glm::vec3 r = glm::normalize(glm::reflect(in, n));
         const float e_uv = this->e->sample(uv);
-        return random::RandomizationHelper::cosineRandomHemispherePDF(glm::dot(r, out), e_uv);
+        const float cosProb = this->computeCosineSamplingProbability(uv);
+        return cosProb*random::RandomizationHelper::cosineRandomHemispherePDF(glm::dot(r, out), e_uv) + 
+          (1.0f - cosProb)*random::RandomizationHelper::cosineRandomHemispherePDF(glm::dot(n, out), 1.0f);
       }
 
     private:
@@ -78,8 +87,26 @@ namespace ray_storm
       textures::Abstract2DTexturePtr<float> e;
 
       float samplingExponent;
-      float specProb;
-      float diffProb;
+
+      /**
+       * @brief      Calculates the cosine sampling probability, that is
+       *             the probability to sample the specular part of the brdf.
+       *
+       * @param[in]  uv    Current texture coordinates.
+       *
+       * @return     The cosine sampling probability.
+       */
+      float computeCosineSamplingProbability(const glm::vec2 &uv)
+      {
+        const float e_uv = this->e->sample(uv);
+        const glm::vec3 kS_uv = this->kS->sample(uv);
+        const glm::vec3 kD_uv = this->kD->sample(uv);
+
+        const float specular = glm::length(kS_uv)*std::max(1.0f, e_uv*0.025f);
+        const float diffuse = glm::length(kD_uv);
+
+        return specular/(specular + diffuse);
+      }
       
     };
   }
